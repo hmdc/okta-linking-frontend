c74b46a3ea4f669caeebcd57a243d7318429a5dc
David Verdú <davidverdu@theagilemonkeys.com>
2025-02-04 18:05:54 +0000

    mimic the most possible the version present in main branch

--- vite.config.js
+++ vite.config.js
@@ -1,55 +1,66 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path';
-import dns from 'dns'
 import dotenv from 'dotenv'
-
-dotenv.config()
-
-dns.setDefaultResultOrder('verbatim')
-
-process.env.CLIENT_ID = process.env.SPA_CLIENT_ID || process.env.CLIENT_ID;
-
-const env = {};
-
-// List of environment variables made available to the app
-[
-  'ISSUER',
-  'CLIENT_ID',
-  'EXT_AUTH_URL',
-  'EXT_CLIENT_ID',
-  'EXT_REDIRECT_URI',
-  'LINK_API',
-  'SUPPORT_EMAIL',
-  'SUPPORT_URL',
-].forEach((key) => {
-  if (!process.env[key]) {
-    throw new Error(`Environment variable ${key} must be set. See README.md`);
-  }
-  env[key] = process.env[key];
-});
+import dns from 'dns'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  define: {
-    'process.env': env
-  },
-  resolve: {
-    alias: {
-      'react-router-dom': path.resolve(__dirname, 'node_modules/react-router-dom')
+export default defineConfig(({ _, mode }) => {
+  dotenv.config({path: '.okta.env'})
+
+  const env = loadEnv(mode, process.cwd(), '')
+
+  process.env.CLIENT_ID = process.env.SPA_CLIENT_ID || process.env.CLIENT_ID
+  Object.assign(env, process.env)
+
+  const filteredEnv = {}
+
+  const variables = [
+      'ISSUER',
+      'CLIENT_ID',
+      'EXT_AUTH_URL',
+      'EXT_CLIENT_ID',
+      'EXT_REDIRECT_URI',
+      'LINK_API',
+      'SUPPORT_EMAIL',
+      'SUPPORT_URL'
+  ]
+
+  variables.forEach((key) => {
+    if (!env[key]) {
+      throw new Error(`Environment variable ${key} must be set in .okta.env`)
     }
-  },
-  server: {
-    port: process.env.PORT || 3000
-  },
-  build: {
-    rollupOptions: {
-      // always throw with build warnings
-      onwarn (warning, warn) {
-        warn('\nBuild warning happened, customize "onwarn" callback in vite.config.js to handle this error.');
-        //throw new Error(warning);
+    filteredEnv[key] = env[key]
+  })
+
+  dns.setDefaultResultOrder('verbatim')
+
+  return {
+    plugins: [react()],
+    resolve: {
+      alias: {
+        'react-router-dom': path.resolve(__dirname, 'node_modules/react-router-dom')
+      }
+    },
+    define: {
+      'process.env': filteredEnv
+    },
+    test: {
+      environment: 'jsdom',
+      globals: true
+    },
+    server: {
+      port: env.PORT || 3000
+    },
+    build: {
+      rollupOptions: {
+        // always throw with build warnings
+        onwarn (warning, warn) {
+          warn('\nBuild warning happened, customize "onwarn" callback in vite.config.js to handle this error.');
+          //throw new Error(warning);
+        }
       }
     }
   }
+
 })
