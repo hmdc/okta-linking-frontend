d655789cb9f98a6947986c7131c57c171be8748e
David Verdú <davidverdu@theagilemonkeys.com>
2025-02-04 13:12:23 +0000

    remove unneeded code from entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,17 +1,7 @@
 #!/bin/sh
 
-# Create a .env file dynamically based on runtime environment variables
-echo "ISSUER=${ISSUER:-defaultValue}" > /app/.env
-echo "CLIENT_ID=${CLIENT_ID:-defaultValue}" >> /app/.env
-echo "EXT_AUTH_URL=${EXT_AUTH_URL:-defaultValue}" >> /app/.env
-echo "EXT_CLIENT_ID=${EXT_CLIENT_ID:-defaultValue}" >> /app/.env
-echo "EXT_REDIRECT_URI=${EXT_REDIRECT_URI:-defaultValue}" >> /app/.env
-echo "LINK_API=${LINK_API:-defaultValue}" >> /app/.env
-echo "SUPPORT_EMAIL=${SUPPORT_EMAIL:-defaultValue}" >> /app/.env
-echo "SUPPORT_URL=${SUPPORT_URL:-defaultValue}" >> /app/.env
-
-# Build application injecting environment variables from .env
+# Build application injecting environment variables
 cd /app && npm run build
 
-# Run the serve command (it requires env variables preprocessed on previous step)
+# Run the serve command
 exec serve -s /app/dist -l 3000
