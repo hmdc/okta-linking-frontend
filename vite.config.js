85175f9c54932ef8221e24648cc5c55ac2f5aad0
Aday Bujeda <aday@theagilemonkeys.com>
2025-02-10 22:01:40 +0000

    Added .okta.env properties loading with dotenv

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,11 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path';
+import dotenv from 'dotenv'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ _, mode }) => {
+  dotenv.config({path: '.okta.env'})
 
   const env = loadEnv(mode, process.cwd(), '')
 
